8acc9bf01135300b102c06faa3764dd057b40406
JellyBoy <pkkann@gmail.com>
2013-09-30 14:05:46 +0200

    pakker

---
+++ R3_ppjr_WebServer/src/webserver/WebServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webserver;
+
+/**
+ *
+ * @author patrick
+ */
+public class WebServer {
+    
+    public WebServer() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
