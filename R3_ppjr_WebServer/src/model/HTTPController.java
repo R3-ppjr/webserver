955bff417a1d429de3d94ecb8781f6860287a6b6
TotalOfferen <ugyhop@hotmail.com>
2013-10-02 11:07:39 +0200

    hej2

--- R3_ppjr_WebServer/src/model/HTTPController.java
+++ R3_ppjr_WebServer/src/model/HTTPController.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -17,11 +18,11 @@ import java.util.logging.Logger;
  *
  * @author patrick
  */
-public class HTTPController implements Runnable{
-    
+public class HTTPController implements Runnable {
+
     private Socket connection;
-    
-    public HTTPController (Socket connection) {
+
+    public HTTPController(Socket connection) {
         this.connection = connection;
     }
 
@@ -29,26 +30,46 @@ public class HTTPController implements Runnable{
     public void run() {
         try {
             BufferedReader inFromClient = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(
-                connection.getOutputStream());
-            
-        String message = inFromClient.readLine();
-        System.out.println(message);
-        
-        String [] splitMessage = message.split(" ");
-        
-        for(int i=0; i<splitMessage.length; i++){
-            System.out.println(splitMessage[i]);
-        }
-        
-        outToClient.writeBytes("HTTP/1.0 404 Not Found");
-        
-        outToClient.close();
-        }
-        catch (IOException ex) {
+                    new InputStreamReader(connection.getInputStream()));
+            OutputStream outToClient = connection.getOutputStream();
+
+            String message = inFromClient.readLine();
+
+            String[] splitMessage = message.split(" ");
+
+            switch (splitMessage[0]) {
+                case "GET":
+                    System.out.println("GET");
+                    break;
+                case "HEAD":
+                    break;
+                case "PUT":
+                    break;
+                case "POST":
+                    break;
+                case "DELETE":
+                    break;
+                case "LINK":
+                    break;
+                case "UNLINK":
+                    break;
+                default:
+                    break;
+            }
+
+//        for(int i=0; i<splitMessage.length; i++){
+//            System.out.println(splitMessage[i]);
+//        }
+
+            outToClient.write("HTTP/1.0 404 Not Found /r/n".getBytes());
+            outToClient.write("/r/n".getBytes());
+
+            outToClient.close();
+        } catch (IOException ex) {
             Logger.getLogger(HTTPController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    public void respond(String responce) {
+    }
 }
