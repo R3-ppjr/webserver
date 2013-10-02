871fe9903a71058cc08b345f1ae71c1faa480b02
TotalOfferen <ugyhop@hotmail.com>
2013-10-02 10:47:29 +0200

    hej

--- R3_ppjr_WebServer/src/model/HTTPController.java
+++ R3_ppjr_WebServer/src/model/HTTPController.java
@@ -4,7 +4,14 @@
  */
 package model;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,7 +27,28 @@ public class HTTPController implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("LORT");
+        try {
+            BufferedReader inFromClient = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(
+                connection.getOutputStream());
+            
+        String message = inFromClient.readLine();
+        System.out.println(message);
+        
+        String [] splitMessage = message.split(" ");
+        
+        for(int i=0; i<splitMessage.length; i++){
+            System.out.println(splitMessage[i]);
+        }
+        
+        outToClient.writeBytes("HTTP/1.0 404 Not Found");
+        
+        outToClient.close();
+        }
+        catch (IOException ex) {
+            Logger.getLogger(HTTPController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
